679a349d584eddd723f4747141bf09979fe3c94e
Amélie Reymond <amelietamrey@gmail.com>
2019-01-06 21:49:17 +0000

    Base set discovery

--- reviewify.py
+++ reviewify.py
@@ -1,3 +1,9 @@
+import os
+from typing import Set
+
 from sets import sets
 
-print("Sets : " + str(sets.get_sets()))
+print("Starting Reviewify with working directory : " + str(os.path.realpath(".")))
+
+review_sets: Set[str] = sets.get_sets_paths()
+print("Found the following review sets : " + str(review_sets))
--- sets/sets.py
+++ sets/sets.py
@@ -1,2 +1,13 @@
-def get_sets():
-    return []
+import os
+from os import listdir
+from os.path import join
+from typing import List, Set
+
+DATASET_FOLDER = "data"
+
+
+def get_sets_paths() -> Set[str]:
+    files_in_data_directory: List[str] = listdir(DATASET_FOLDER)
+    tsv_sets = filter(lambda fname: fname.endswith(".tsv"), files_in_data_directory)
+    base_path_of_sets = join(os.path.abspath("."), DATASET_FOLDER)
+    return set(map(lambda set_name: join(base_path_of_sets, set_name), tsv_sets))
