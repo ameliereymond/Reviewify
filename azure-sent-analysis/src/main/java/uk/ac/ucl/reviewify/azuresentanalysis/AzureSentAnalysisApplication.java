4fee5919f14555cd1ad3e0ece26bb40bf2ad7259
Amélie Reymond <amelietamrey@gmail.com>
2019-01-07 17:42:01 +0000

    Use java for azure analytics

---
+++ azure-sent-analysis/src/main/java/uk/ac/ucl/reviewify/azuresentanalysis/AzureSentAnalysisApplication.java
@@ -0,0 +1,14 @@
+package uk.ac.ucl.reviewify.azuresentanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSentAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSentAnalysisApplication.class, args);
+	}
+
+}
+
