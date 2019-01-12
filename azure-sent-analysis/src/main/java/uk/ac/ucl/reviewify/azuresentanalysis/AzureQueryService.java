4fae7703991df9734486bbd049614324bddef617
Amélie Reymond <amelietamrey@gmail.com>
2019-01-12 13:58:01 +0000

    Add query splitting for datasets

--- azure-sent-analysis/src/main/java/uk/ac/ucl/reviewify/azuresentanalysis/AzureQueryService.java
+++ azure-sent-analysis/src/main/java/uk/ac/ucl/reviewify/azuresentanalysis/AzureQueryService.java
@@ -1,11 +1,17 @@
 package uk.ac.ucl.reviewify.azuresentanalysis;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -20,6 +26,8 @@ import uk.ac.ucl.reviewify.azuresentanalysis.types.azure.UnreviewedDocument;
 
 @Component
 public class AzureQueryService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AzureQueryService.class);
 
     private static final ParameterizedTypeReference<Map<String, List<UnreviewedDocument>>> QUERY_TYPE = new ParameterizedTypeReference<>() {
     };
@@ -39,6 +47,36 @@ public class AzureQueryService {
     }
 
     List<ReviewedDocument> queryAzureFor(List<UnreviewedDocument> documentsToReview) {
+        if (documentsToReview.size() <= 100) {
+            return queryPart(documentsToReview);
+        }
+
+        LOGGER.info("Queried analysis of more than 100 documents. Splitting requests by batches of 100.");
+
+        final List<UnreviewedDocument> remaining = new LinkedList<>(documentsToReview);
+        final List<List<UnreviewedDocument>> azureInputs = new ArrayList<>(documentsToReview.size() / 100 + 1);
+
+        while (!remaining.isEmpty()) {
+            final List<UnreviewedDocument> subset = new ArrayList<>(100);
+            for (int i = 0; i < 100; i++) {
+                subset.add(remaining.remove(0));
+            }
+            azureInputs.add(subset);
+        }
+
+        LOGGER.info("Will need {} queries to go through whole dataset.", azureInputs.size());
+
+        final List<ReviewedDocument> analyzedDocuments = IntStream.range(0, azureInputs.size()).mapToObj(queryId -> {
+            LOGGER.info("\t -> Query {}/{}", queryId, azureInputs.size());
+            return queryPart(azureInputs.get(queryId));
+        }).flatMap(List::stream).collect(Collectors.toList());
+
+        LOGGER.info("Done analyzing {} documents! Result size : {}", documentsToReview.size(), analyzedDocuments.size());
+
+        return analyzedDocuments;
+    }
+
+    private List<ReviewedDocument> queryPart(List<UnreviewedDocument> documentsToReview) {
         final Map<String, List<UnreviewedDocument>> docs = new HashMap<>();
         docs.put("documents", documentsToReview);
 
