7a0cca17e96511fcd0a5db13fc635ec78247c310
Amélie Reymond <amelietamrey@gmail.com>
2019-01-14 20:15:08 +0000

    Base documentation

--- reviewify.py
+++ reviewify.py
@@ -6,27 +6,30 @@ from safetychecks import safety_check
 from sets import findsets, loadsets
 from sets.CustomerReview import CustomerReview
 from sets.reviews import ReviewSet
-from stats import country_star_stats, country_sentiment
+from stats import country_sentiment
 from stats.country_sentiment import CountrySentimentStats
 from stats.country_star_stats import CountryStarStats
 from visualization import mp
 
 print("Starting Reviewify with working directory : " + str(os.path.realpath(".")))
 
+# Look into folder to find out what sets are available
 review_sets_paths: Set[str] = findsets.find_sets_paths()
 print("Found the following review sets : " + str(review_sets_paths))
 
+# Find out shortest set's length
 min_set_len = min(list(map(lambda rs: loadsets.set_size(rs), review_sets_paths)))
 print("Shortest set has " + str(min_set_len) + " lines. Only getting that much from each.")
 
+# Load sets stopping at the number of reviews in the shortest set to get equal set lengths
 review_sets_loaded: Set[ReviewSet] = set()
 for review_set_path in review_sets_paths:
     review_set_loaded = loadsets.load_set(review_set_path, min_set_len)
     review_sets_loaded.add(review_set_loaded)
 print("Loaded the following review sets : " + str(list(map(str, review_sets_loaded))))
 
+# Keep only basic infos about a set's reviews after cleanup
 cleaned_sets: Dict[str, List[CustomerReview]] = {}
-
 for review_set in review_sets_loaded:
     print("Set : " + review_set.marketplace())
     marketplace_name: str = review_set.marketplace()
@@ -36,36 +39,33 @@ for review_set in review_sets_loaded:
     cleaned_sets[marketplace_name] = safety_check(marketplace_reviews)
     print("\t-> Finished importing set " + marketplace_name)
 
+# Get stats of stars per country
 star_stats_per_country: Dict[str, CountryStarStats] = {}
 for country in cleaned_sets.keys():
     cleaned_set_country: List[CustomerReview] = cleaned_sets[country]
     print("Star ratings : " + country)
     star_stats_per_country[country] = star_ratings.ratinglist(cleaned_set_country)
 
-
-fr_star_stats: CountryStarStats = country_star_stats.from_review_set(cleaned_sets["FR"])
-print(fr_star_stats)
-de_star_stats: CountryStarStats = country_star_stats.from_review_set(cleaned_sets["DE"])
-us_star_stats: CountryStarStats = country_star_stats.from_review_set(cleaned_sets["US"])
-uk_star_stats: CountryStarStats = country_star_stats.from_review_set(cleaned_sets["UK"])
-
+# Get stats of sentiment per country
 sentiment_stats_per_country: Dict[str, CountrySentimentStats] = {}
 for country in cleaned_sets.keys():
     cleaned_set_country: List[CustomerReview] = cleaned_sets[country]
     sentiment_stats_per_country[country] = country_sentiment.from_review_set(cleaned_set_country)
 
+# Boxplot star values per country
 star_values_per_country: Dict[str, List[int]] = {}
 for country in cleaned_sets.keys():
     star_values_per_country[country] = list(map(lambda a_review: a_review.star_rating, cleaned_sets[country]))
+mp.boxplot_with_label(star_values_per_country, "Stars by country")
 
+# Boxplot sentiment values per country
 sentiment_values_per_country: Dict[str, List[int]] = {}
 for country in cleaned_sets.keys():
     sentiment_values_per_country[country] = list(
         map(lambda a_review: a_review.sentiment_analysis_score, cleaned_sets[country]))
-mp.layered_histogram(star_values_per_country, "Stars by country")
-mp.layered_histogram(sentiment_values_per_country, "Sentiment values per country")
+mp.boxplot_with_label(sentiment_values_per_country, "Sentiment values per country")
 
-# Box plots for each star rating
+# Get reviews classified by star values
 reviews_by_stars: Dict[int, List[CustomerReview]] = {}
 for country in cleaned_sets.keys():
     for review in cleaned_sets[country]:
@@ -73,12 +73,12 @@ for country in cleaned_sets.keys():
             reviews_by_stars[review.star_rating] = []
         reviews_by_stars[review.star_rating].append(review)
 
-sentiment_scores_per_stars: Dict[int, List[float]] = {}
+# Boxplot sentiment score per stars
+sentiment_scores_per_stars: Dict[str, List[float]] = {}
 for star_count in reviews_by_stars.keys():
-    sentiment_scores_per_stars[star_count] = list(map(
+    sentiment_scores_per_stars[str(star_count)] = list(map(
         lambda a_review: a_review.sentiment_analysis_score,
         reviews_by_stars[star_count]
     ))
+mp.boxplot_with_label(sentiment_scores_per_stars, "Sentiment scores by stars")
 
-
-mp.layered_histogram_int(sentiment_scores_per_stars, "Sentiment scores by stars")
--- visualization/mp.py
+++ visualization/mp.py
@@ -4,17 +4,7 @@ import pandas
 from matplotlib import pyplot
 
 
-def layered_histogram(elements_names_to_list_of_values: Dict[str, List[float]], plotname: str):
-    temp = {}
-    for k in elements_names_to_list_of_values.keys():
-        temp[k] = pandas.Series(elements_names_to_list_of_values[k])
-    frame = pandas.DataFrame.from_dict(temp)
-    frame.boxplot()
-    pyplot.title(plotname)
-    pyplot.show()
-
-
-def layered_histogram_int(elements_names_to_list_of_values: Dict[int, List[float]], plotname: str):
+def boxplot_with_label(elements_names_to_list_of_values: Dict[str, List[float]], plotname: str):
     temp = {}
     for k in elements_names_to_list_of_values.keys():
         temp[k] = pandas.Series(elements_names_to_list_of_values[k])
